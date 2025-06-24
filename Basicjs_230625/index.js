d14060d5e1e772a9990e3eda745ac5963091bf87
vaibhav verma <vaibhavverma6393@gmail.com>
2025-06-24 14:44:07 +0530

    Day 9

---
+++ Basicjs_230625/index.js
@@ -0,0 +1,75 @@
+//string in  javascript
+
+// let text = "John Doe";
+// console.log(text);
+
+// in javascript string is work in single quotes and double quotes 
+// let name1 = "Vaibhav Verma";  // Double Quotes
+// let name2 = 'Vaibhav Verma';  //Single Quotes
+// console.log(name1);
+// console.log(name2);
+
+// In java script we can use qoutes in side of sting but it nessary 
+// that it is not match to  surrounding quotes of full string
+
+// let a = "It's alright";
+// let b = "He is called 'vaibhav'";
+// let c = 'He is called "vaibhav"';
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// Template of Strings
+// we use template as :- `This is content of strinf`
+
+// let tex= `He's called "vaibhav"`;
+// console.log(tex);
+
+// length of string 
+// let name="vaibhav"
+// let num=name.length
+// console.log(num);
+
+
+// Adding of two String
+// let firstname ="vaibhav "
+// let lastname ="verma"
+// let fullname=firstname + lastname
+// console.log(fullname);
+
+// // string can not create using new keyword
+// // if we use new keyward than its make a class /object
+// let x= "vaibhav";
+// let y =new String("vaibhav");
+// console.log(typeof(x));
+// console.log(typeof(y));
+
+// // if we are compare string and  object
+// console.log(x==y); // here the code are compareing the content of x and y
+// console.log(x===y); // here the code are also compare the data  type of the x ans y
+
+
+// if we are compare two object then it will always return false 
+// let a=new String("vaibhav");
+// let b=new String("vaibhav");
+// console.log(a===b);
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// JavaScript String Methods
+
+
+// string Length method
+// let name = "vaibhav verma";  // here the space is counted in length
+// let num = name.length;
+// console.log(num);
+
+// Extracting String Characters 
+// there are three types 
+
+
+// let text="vaibhav verma";
+// let a=text.at(0)
+// console.log(a);
